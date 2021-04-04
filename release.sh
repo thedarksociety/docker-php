c960bee13e4a6520c8e5c797e1b0daa421996f12
Rye Miller <millerrye17@gmail.com>
2021-04-04 02:31:10 -0600

    Addition of support for CI images and GitHub Actions builds.

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Release script for Github Actions
+# Version: 0.1 [2020-11-28]
+#
+# - Build and push Docker containers from a Github
+#   repository on a release.
+#
+# Author: Dark Society <developers@darksociety.cloud>
+# Copyright (c) 2021, The Dark Society
+
